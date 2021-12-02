8bcc0eb46b968f7b56744376eb4e013974696724
Christopher Lewerenz <c.lewerenz@mail.de>
2021-12-02 01:30:25 +0100

    added cors origin

--- thbStorage_API/settings.py
+++ thbStorage_API/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'backend',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'thbStorage_API.urls'
