4fef52970b3e877cae97b6407be934e7c6c24372
ClemensDel <clemens.delbrueck@gmail.com>
2021-12-16 03:42:30 +0100

    rename file

--- backend/urls.py
+++ backend/urls.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken import views as v
 from backend import views
 
 router = routers.SimpleRouter()
-#router.register(r'files', views.FileViewSet)
+# router.register(r'files', views.FileViewSet)
 # TODO authentication router
 
 urlpatterns = [
@@ -13,6 +13,7 @@ urlpatterns = [
     path('files/', views.files_list),
     path('file/upload', views.upload_file),
     path('file', views.get_file),
+    path('filename/update', views.update_filename),
     # API-Endpoint to obtain user token with cred
     path('api-token-auth/', v.obtain_auth_token)
 
