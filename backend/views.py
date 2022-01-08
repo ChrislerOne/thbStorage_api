61662fbab38c92778ab09a9ca695b2566b221e52
ClemensDel <clemens.delbrueck@gmail.com>
2022-01-08 13:24:45 +0100

    Bugfix

--- backend/views.py
+++ backend/views.py
@@ -345,7 +345,7 @@ def rename_directory(request):
     else:
         return Response(data={'status': 'directory not exists'}, status=status.HTTP_400_BAD_REQUEST)
 
-    rec_list = FileNewModel.objects.get(location=location, owner_id=owid)
+    rec_list = list(FileNewModel.objects.get(location=location, owner_id=owid))
 
     try:
         for obj in FileNewModel.objects.get(location=location, owner_id=owid):
