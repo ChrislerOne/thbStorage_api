bf4208088285ed336a1182af05b2575a00fed4a7
ClemensDel <clemens.delbrueck@gmail.com>
2022-01-08 14:40:26 +0100

    Changes by clewe

--- backend/views.py
+++ backend/views.py
@@ -357,6 +357,7 @@ def rename_directory(request):
                                                owner_id=owid):
             print(obj)
             if rename_content_file_directory_in_DB(obj, new_absolute_location) is False:
+                print(Exception)
                 raise Exception
 
     except:
